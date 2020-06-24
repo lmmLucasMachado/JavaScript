760640a99d3975f170e107ca3b8eeaea79dbdf06
lmmLucasMachado <lmlucasmachadom@gmail.com>
2020-06-24 02:31:42 -0300

    Utilizando axios no lugar do xmlhttprequest

--- 4_JS_assincrono/main.js
+++ 4_JS_assincrono/main.js
@@ -1,27 +1,8 @@
-var myPromisse = function(){
-    return new Promise(function (resolve,reject){
-        var xhr = new XMLHttpRequest();
-        
-        xhr.open('Get', 'https://api.github.com/users/lmmlucasMachado');
-        xhr.send(null);
-        
-        xhr.onreadystatechange = function(){
-            if (xhr.readyState == 4 ){
-                if (xhr.status == 200){
-                    resolve(JSON.parse(xhr.responseText));
-                }else{
-                    reject('Erro na requisição.');
-                }  
-            }
-        }
+axios.get('https://api.github.com/users/lmmlucasMachado')
 
-    });
-};
-
-myPromisse()
     .then(function(response){
-        console.log(response);
+        console.log(response.data.avatar_url);
     })
     .catch(function(error){
         console.warn(error);
-});
+    })
