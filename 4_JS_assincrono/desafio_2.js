5dfe90160affe1c76a731f0d1d2eb72a63b26da2
lmmLucasMachado <lmlucasmachadom@gmail.com>
2020-06-25 05:48:14 -0300

    App com requisição

--- 4_JS_assincrono/desafio_2.js
+++ 4_JS_assincrono/desafio_2.js
@@ -2,6 +2,7 @@ var bodyElement= document.querySelector("body");
 
 var inputElement = document.createElement("input");
 var buttonElement = document.createElement("button");
+var listElement = document.createElement("ul");
 
 inputElement.setAttribute('type', 'text');
 inputElement.setAttribute('name', 'user');
@@ -12,18 +13,43 @@ buttonElement.appendChild(textButton);
 
 bodyElement.appendChild(inputElement);
 bodyElement.appendChild(buttonElement);
+bodyElement.appendChild(listElement);
 
-buttonElement.onclick = function pesquisaGit(){
-    var userGit = inputElement.value;
+var reposGit = JSON.parse(localStorage.getItem('listTodos')) || [];
 
-    axios.get('https://api.github.com/users/'+userGit)
-    
-        .then(function(response){
-            console.log();
-        })
-        .catch(function(error){
-            console.warn(error);
-        })
-    inputElement.value = "";
+function renderUsers(){
+    listElement.innerHTML = '';
+    for(userGit of reposGit){
+        var userGitElement = document.createElement("li");
+        var userGitText = document.createTextNode(userGit);
+        
+        userGitElement.appendChild(userGitText);
+        listElement.appendChild(userGitElement);
+    }
 }
 
+renderUsers();
+
+function adduserGit(){
+    var userGit = inputElement.value;
+    
+    inputElement.value = "";
+        
+    axios.get('https://api.github.com/users/'+userGit+'/repos')
+    
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        alert("nome de usuário não encotrado")
+    })
+    
+    renderUsers();
+    saveStorage();
+}
+
+buttonElement.onclick = adduserGit;
+
+function saveStorage(){
+    localStorage.setItem('listreposGit', JSON.stringify(reposGit));
+}
