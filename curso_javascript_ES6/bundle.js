4970b70d3cc6b125926d5d2c6930f2815d42577f
lmmLucasMachado <lmlucasmachadom@gmail.com>
2020-06-29 03:58:06 -0300

    Conceitos

--- curso_javascript_ES6/bundle.js
+++ curso_javascript_ES6/bundle.js
@@ -1,3 +1,44 @@
 "use strict";
 
-alert("42");
+var array = [1, 3, 4, 5, 7, 8, 10];
+var newArrayDobroItem = array.map(function (item) {
+  return item * 2;
+});
+console.log(newArrayDobroItem);
+var newArrayMaisPosicao = array.map(function (item, index) {
+  return item + index;
+});
+console.log(newArrayMaisPosicao);
+var sum = array.reduce(function (item, next) {
+  return item + next;
+});
+console.log(sum);
+var filter = array.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter);
+var find = array.find(function (item) {
+  return item == 4;
+});
+console.log(find);
+var user = {
+  name: "Lucas",
+  age: 23,
+  address: {
+    city: "Ceilandia",
+    state: "DF"
+  }
+};
+
+function showName(_ref) {
+  var name = _ref.name,
+      age = _ref.age;
+  console.log(name, age);
+}
+
+showName(user);
+var _user$address = user.address,
+    city = _user$address.city,
+    state = _user$address.state;
+console.log(city);
+console.log(state);
--- curso_javascript_ES6/operacoesArray.js
+++ curso_javascript_ES6/operacoesArray.js
@@ -1,8 +1,13 @@
 const array = [1,3,4,5,7,8,10];
 
+//Arrow function
+/*
 const newArrayDobroItem = array.map(function(item){
     return item *2
 })
+é similar a função abaixo
+*/
+const newArrayDobroItem = array.map(item => item * 2 )
 
 console.log(newArrayDobroItem)
 
@@ -29,22 +34,3 @@ const find = array.find(function(item){
 
 console.log(find);
 
-const user = {
-    name: "Lucas",
-    age: 23,
-    address: {
-        city: "Ceilandia",
-        state: "DF"
-    }
-}
-
-function showName({name, age}){
-    console.log(name, age);
-}
-
-showName(user);
-
-const {address: {city,state}} = user;
-
-console.log(city);
-console.log(state);
