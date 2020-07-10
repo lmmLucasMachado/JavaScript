0b1d137bd32c550852b1cda1b22717e777744e63
lmmLucasMachado <lmlucasmachadom@gmail.com>
2020-07-09 23:24:22 -0300

    Adicionar resositorio

--- curso_javascript_ES6/aplicacao_es6/src/main.js
+++ curso_javascript_ES6/aplicacao_es6/src/main.js
@@ -1,1 +1,29 @@
-alert('42');
+class App{
+    constructor(){
+        this.repositories = [];
+
+        this.formRepo = document.getElementById("repo-form");
+
+        this.registerRepo();
+    }
+
+    registerRepo(){
+        this.formRepo.onsubmit = event => this.addRepository(event);
+        addRepository(event);
+    }
+    
+    addRepository(){
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'Rocketseat.com.br',
+            description:'Tire a sua ideia do papel e de vida a uma startup',
+            avatar_url:'https://avatars0.githubusercontent.com/u/28929274?s=400&v=4',
+            html_url:'http://https://github.com/Rocketseat '
+        })
+    
+        console.log(repositories);
+    }
+}
+
+App();
